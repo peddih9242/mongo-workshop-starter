32fb47e7bc72205028381fd8cbb8787e826d11bd
peddih9242 <78389729+peddih9242@users.noreply.github.com>
2024-05-11 15:16:30 +1200

    almost working contacts app

---
+++ backend/src/data/schema.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const contactSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    phoneNumber: String,
+    photoUrl: String,
+    funFact: String
+})
+
+export const Contact = mongoose.model("Contacts", contactSchema);
+
